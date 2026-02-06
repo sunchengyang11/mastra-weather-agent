685db4430bc24b5b6fd8c48d5df452d18aae3ebe
sunchengyang11 <chengyang.c.sun@gmail.com>
2026-02-06 14:08:49 +0900

    Update index.ts

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -4,7 +4,7 @@ import { workflows } from "./workflows";
 import { tools } from "./tools";
 import { scorers } from "./scorers";
 
-export default createMastra({
+export const mastra = createMastra({
   agents,
   workflows,
   tools,
